6f8fa46a740dffa6e7f43f94e589e26945c18215
kamneklogs <kamneklogs@gmail.com>
2021-09-09 16:26:15 -0500

    First approach

--- src/main.py
+++ src/main.py
@@ -1,19 +1,20 @@
-from selenium import webdriver  # Para controlar chromium, automatizada
-from bs4 import BeautifulSoup  # Scrapping mediante localización de componentes
-import pandas as pd  # Pandas, just pandas jaj
-
-
-# Directorio de chromium con chromedriver (Ext descargable, verificar compatibilidad de estensión y browser)
-driver = webdriver.Chrome("/usr/lib64/chromium-browser/chromedriver")
+import requests
+from bs4 import BeautifulSoup as bs
 
 products = []  # Lista para guardar nombres de los autos
 prices = []  # Lista para guardar precios
 
+url = "https://loscoches.com/carros-usados/"
 
-# https://loscoches.com/carros-usados/ Pagina objetivo
-# car-content   class de nombre de autos en el html
-# new-price    class de precios "                   "
+response = requests.get(url)
+html = bs(response.text, 'html.parser')
 
-driver.get("https://loscoches.com/carros-usados/")  # Manipulación de chromium
-content = driver.page_source  # Referencia a fuente de la pagina
-soup = BeautifulSoup(content)
+vehicles_html = html.find_all('div', 'a.text', class_='car-content')
+
+for vehicle in vehicles_html:
+    item = vehicle.text.split('$')
+    products.append(item[0])
+    prices.append('$'+item[1])
+
+for i in range(len(products)):
+    print(products[i].strip() + ";" + prices[i])
