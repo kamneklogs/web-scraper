499d46648847ec87718b6f2ddb51d36dd92d1bec
kamneklogs <kamneklogs@gmail.com>
2021-09-12 13:16:56 -0500

    beta version

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 
-products = []  # Lista para guardar nombres de los autos
-prices = []  # Lista para guardar precios
+products = []  
+prices = []  
+years = []  
+kilometers = [] 
+
 
 url = "https://loscoches.com/carros-usados/?cars_pp=48&cars_orderby=date&cars_order=asc&lay_style=view-grid-left&cars_grid=yes"
 
@@ -12,30 +15,27 @@ html = bs(response.text, 'html.parser')
 
 vehicles_html = html.find_all('div', 'a.text', class_='car-content')
 
-vehicles_html
-
-
-# col-lg-4 col-md-4 col-sm-4 col-xs-6
-# col-lg-4 col-md-4 col-sm-4 col-xs-6
-# col-lg-4 col-md-4 col-sm-4 col-xs-6
-
-entradas = html.find_all(
+inputs = html.find_all(
     'div', {'class': 'col-lg-4 col-md-4 col-sm-4 col-xs-6'})
-entradas
-
-len(entradas)
 
 
-for i, entrada in enumerate(entradas):
-    titulo = entrada.find(
-        'div', class_='car-content').getText().split('$')[0].strip()
+for i, entrada in enumerate(inputs):
 
-    precio = entrada.find(
-        'div', class_='car-content').getText().split('$')[1].strip()
+    products.append(entrada.find(
+        'div', class_='car-content').getText().split('$')[0].strip())
+
+    prices.append(entrada.find(
+        'div', class_='car-content').getText().split('$')[1].strip())
 
     anioYkilometraje = entrada.find(
         'ul', class_='list-inline').getText().strip().split(' ')
-    anio = anioYkilometraje[0]
-    kilometraje = anioYkilometraje[1]
 
-    print('{};{};{};{}'.format( titulo, precio, anio, kilometraje))
+    years.append(anioYkilometraje[0])
+
+    kilometers.append(anioYkilometraje[1])
+
+
+df = pd.DataFrame({'Model': products, 'Price': prices,
+                  'year': years, 'kms': kilometers})
+
+df.to_csv('cars.csv', index=False, encoding='utf-8')
